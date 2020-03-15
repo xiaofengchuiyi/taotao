69185dd74ed8b2d2996f30d4dd959f79f443370d
shanbo <305765986@qq.com>
2020-03-15 20:29:38 +0800

    test

---
+++ taotao-manager/taotao-manager-dao/src/main/java/com/chinapost/App.java
@@ -0,0 +1,13 @@
+package com.chinapost;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
